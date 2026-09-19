0b83a1e8ed7e609b6ac02c8be2b8ba2cdd39b889
awareermine <aramie.ewen@gmail.com>
2026-09-19 15:29:42 -0400

    rename var

--- backend/src/backend/server.py
+++ backend/src/backend/server.py
@@ -1,7 +1,7 @@
 import socket
 import json
 
-IP_ADDR  ="127.0.0.1"
+HOST  ="127.0.0.1"
 PORT = 5005
 
 def server() -> None:
@@ -10,7 +10,7 @@ def server() -> None:
     # TODO: add client that listens for audio data to send?
     obj = {"hello": "world"}
     print("SERVER: SENDING MESSAGE")
-    sock.sendto(json.dumps(obj).encode('utf-8'), (IP_ADDR, PORT)) # we will want JSON for the front-end
+    sock.sendto(json.dumps(obj).encode('utf-8'), (HOST, PORT)) # we will want JSON for the front-end
 
     sock.close()
 
