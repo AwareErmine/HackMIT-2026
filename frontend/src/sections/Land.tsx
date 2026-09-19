0f78e00916340e26c52d9d56ff51f62c0089d1c4
awareermine <aramie.ewen@gmail.com>
2026-09-19 16:16:42 -0400

    begin drawer

--- frontend/src/sections/Land.tsx
+++ frontend/src/sections/Land.tsx
@@ -1,7 +1,25 @@
-type LandProps = {
-  className?: string;
-};
+import { useState } from "react";
 
-export default function Land({ className }: LandProps) {
-  return <div className={className}></div>;
+export default function Land() {
+  const [hidden, setHidden] = useState(true);
+
+  return (
+    <>
+      <button
+        onClick={() => setHidden(!hidden)}
+        className="absolute z-10 right-[50%] px-5 pt-2 rounded-t-full bg-amber-50 transition-all"
+        style={{
+          bottom: hidden ? "0" : "33%",
+        }}
+      >
+        land
+      </button>
+      <div
+        className="absolute z-1 bottom-0 w-screen h-[33%] transition-all bg-amber-50"
+        style={{
+          bottom: hidden ? "-33%" : "0",
+        }}
+      ></div>
+    </>
+  );
 }
