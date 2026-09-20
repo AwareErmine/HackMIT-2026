3337e45549f22ccbaaf21f2abbf919700baedc6d
awareermine <aramie.ewen@gmail.com>
2026-09-20 07:47:32 -0400

    get data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import { useEffect } from "react";
 import Land from "./sections/Land";
 import Water from "./sections/Water";
 import Sky from "./sections/Sky";
+import { useFishStore } from "./store";
+import { getFishes } from "./data";
 
 function App() {
+  const setFishes = useFishStore((state) => state.setFishes);
+
   useEffect(() => {
-    // TODO: connect to socket server!
-  });
+    getFishes().then(setFishes);
+  }, [setFishes]);
 
   return (
     <div className="overflow-clip h-screen relative">
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { putFish } from "./data";
 
 export type Fish = {
   volume: number; // range from 0-100 (percent from bottom)
@@ -11,35 +12,16 @@ type State = {
 };
 
 type Action = {
-  addFish: (f: Fish) => void;
-  removeFish: (fid: Fish["id"]) => void;
+  setFishes: (f: Fish[]) => void;
   moveFish: (fid: Fish["id"], volume: number, left: number) => void;
   getFish: (fid: Fish["id"]) => Fish | undefined;
 };
 
 export const useFishStore = create<State & Action>((set, get) => ({
-  fishes: [
-    { volume: 10, id: "Anya", left: Math.random() * window.innerWidth * 0.8 },
-    { volume: 10, id: "Aramie", left: Math.random() * window.innerWidth * 0.8 },
-    { volume: 10, id: "Joyce", left: Math.random() * window.innerWidth * 0.8 },
-    {
-      volume: 20,
-      id: "Rebecca",
-      left: Math.random() * window.innerWidth * 0.8,
-    },
-  ],
+  fishes: [],
+  setFishes: (f) => set(() => ({ fishes: f })),
   getFish: (fid) => get().fishes.find((f) => f.id == fid),
-  addFish: (f) =>
-    set(({ fishes }) => ({
-      fishes: [...fishes, f].sort((a, b) => a.id.localeCompare(b.id)),
-    })),
-  removeFish: (fid) =>
-    set(({ fishes }) => ({
-      fishes: fishes
-        .filter((f) => f.id !== fid)
-        .sort((a, b) => a.id.localeCompare(b.id)),
-    })),
-  moveFish: (fid, volume, left) =>
+  moveFish: (fid, volume, left) => {
     set(({ fishes }) => {
       const fish = fishes.find((f) => f.id == fid);
       return {
@@ -54,5 +36,8 @@ export const useFishStore = create<State & Action>((set, get) => ({
             ].sort((a, b) => a.id.localeCompare(b.id))
           : fishes,
       };
-    }),
+    });
+
+    putFish({ id: fid, volume, left });
+  },
 }));
